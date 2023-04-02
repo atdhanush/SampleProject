2bd7215e3dc2a9686a375b28bda32cacf01ba308
atdhanush <atulasidhanush@gmail.com>
2023-04-02 17:46:02 +0530

    Second Commit

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,5 @@
 public class Practice {
-    public static void main (String Args[]){
+    public static void main (String[] Args){
         for (int i=0;i<5;i++) {
             System.out.println("Numbers are :"+i);
         }
